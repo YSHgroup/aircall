f81fa8f3c87718f14fbd3c14abe099dd1bc34466
YSHgroup <david.dev100@outlook.com>
2023-11-28 14:19:28 -0500

    restore original project

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './style/body.css';
+import './style/app.css';
+import './style/header.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
